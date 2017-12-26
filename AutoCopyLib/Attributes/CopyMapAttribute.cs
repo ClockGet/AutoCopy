7256671f122dc3998d01ff1633369c55c85671c6
ClockGet <597575308@qq.com>
2017-12-27 00:07:57 +0800

    add CopyMapAttribute.cs

---
+++ AutoCopyLib/Attributes/CopyMapAttribute.cs
@@ -0,0 +1,23 @@
+﻿using AutoCopyLib.Utilities;
+using System;
+
+namespace AutoCopyLib.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class CopyMapAttribute : Attribute
+    {
+        private string _mapName;
+        public CopyMapAttribute(string mapName)
+        {
+            Guard.ArgumentNotNullOrEmpty(mapName, mapName);
+            _mapName = mapName;
+        }
+        public string MapName
+        {
+            get
+            {
+                return _mapName;
+            }
+        }
+    }
+}
