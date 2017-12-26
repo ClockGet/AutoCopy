7043f3bb86f6393403012ee5d6acdb3874292979
ClockGet <597575308@qq.com>
2017-12-27 00:11:07 +0800

    use a class wrapped the 3 parameters of delegate

---
+++ AutoCopyLib/ParameterTuple.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace AutoCopyLib
+{
+    public partial class AutoCopy<TSource, TDest>
+    {
+        public class ParameterTuple
+        {
+            public ParameterTuple(ParameterExpression p1, ParameterExpression p2, ParameterExpression p3)
+            {
+                Destination = p1;
+                Source = p2;
+                ErrorMsg = p3;
+            }
+            public ParameterExpression Destination { get; }
+            public ParameterExpression Source { get; }
+            public ParameterExpression ErrorMsg { get; }
+            public IEnumerable<ParameterExpression> Collect()
+            {
+                yield return Destination;
+                yield return Source;
+                yield return ErrorMsg;
+            }
+        }
+    }
+}
